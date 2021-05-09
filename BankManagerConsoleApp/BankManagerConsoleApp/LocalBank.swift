f9ecd7cbb2d1f085c178cccecdc777f8f466ee69
fezravien <yjaewoong@naver.com>
2021-05-09 16:06:44 +0900

    refactor :  LocalBankTask를 LocalBank에 종속
    LocalBankTask가 LocalBank의 소유라는 판단으로 종속시킴
    - LocalBank에서 우선순위를 주는 방식에서 LocalBankTask가 하도록 함
    - LocalBankTask의 초기화 시점에서 상위 클래스를(Operation) 이용해 초기화
    - 랜덤으로 Operation 우선순위를 할당

--- BankManagerConsoleApp/BankManagerConsoleApp/LocalBank.swift
+++ BankManagerConsoleApp/BankManagerConsoleApp/LocalBank.swift
@@ -34,15 +34,42 @@ final class LocalBank {
     private func receiveClient() -> Int {
         return Int.random(in: 10...30)
     }
-    
-    private func assignPriority(_ task: LocalBankTask) {
-        switch task.creditRate {
-        case .vvip:
-            return task.queuePriority = .high
-        case .vip:
-            return task.queuePriority = .normal
-        case .normal:
-            return task.queuePriority = .low
+}
+
+extension LocalBank {
+    class LocalBankTask: Operation {
+        let waitingNumber: UInt
+        let creditRate: CreditRating
+        let workType: WorkType
+        init?(_ number: UInt) {
+            guard let credit = CreditRating.allCases.shuffled().first,
+                  let work = WorkType.allCases.shuffled().first else {
+                return nil
+            }
+            
+            self.waitingNumber = number
+            self.creditRate = credit
+            self.workType = work
+            super.init()
+            super.queuePriority = creditRate.priority
+        }
+
+        override func main() {
+            work(number: waitingNumber, rating: creditRate, type: workType)
+        }
+        
+        private func work(number waitNumber: UInt, rating credit: CreditRating, type taskType: WorkType) {
+            switch taskType {
+            case .deposit:
+                print("\(waitNumber)번 \(credit)고객 \(taskType.description) 시작")
+                Thread.sleep(forTimeInterval: taskType.duration)
+                print("\(waitNumber)번 \(credit)고객 \(taskType.description) 완료")
+            case .loan:
+                let loanProcess = WorkType.LoanProcess.self
+                print("\(waitNumber)번 \(credit)고객 \(loanProcess.loanExecution) 시작")
+                HeadBank.shared.serveClient(number: waitNumber, rating: credit, type: taskType)
+                print("\(waitNumber)번 \(credit)고객 \(loanProcess.loanExecution) 완료")
+            }
         }
     }
 }
